e26d5d339f0d34e0cde747214b2f340824846fd2
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-03-18 22:13:51 -0400

    add logistic regression model,  dump models

--- python/quiz_03/q1.py
+++ python/quiz_03/q1.py
@@ -1,7 +1,9 @@
 import glob
 import cv2 as cv
 import numpy as np
+from joblib import dump, load
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
@@ -37,8 +39,18 @@ x_train, x_test, y_train, y_test = preprocess_data()
 clf = KNeighborsClassifier()
 clf.fit(x_train, y_train)
 
-# Evaluation
-predictions = clf.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
+lr = LogisticRegression(max_iter=10000)
+lr.fit(x_train, y_train)
 
-print(f"accuracy: {accuracy}")
+# Evaluation
+clf_predictions = clf.predict(x_test)
+knn_accuracy = accuracy_score(y_test, clf_predictions)
+
+lr_predictions = lr.predict(x_test)
+lr_accuracy = accuracy_score(y_test, lr_predictions)
+
+print(f"knn accuracy: {knn_accuracy}, logistic regression accuracy: {lr_accuracy}")
+
+# Save models
+dump(clf, "knn_classifier.z")
+dump(lr, "logistic_regression.z")
