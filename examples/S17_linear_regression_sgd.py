b261a205cb3eaf1bf39fc6da6bef4b43911e39a5
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-15 05:52:03 -0400

    Update S17_linear_regression_sgd.py

--- examples/S17_linear_regression_sgd.py
+++ examples/S17_linear_regression_sgd.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+# from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -21,11 +21,11 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # MODEL
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=["Coefficient"])
-print(coeff_df)
-print(regressor.intercept_)
+# regressor = LinearRegression()
+# regressor.fit(X_train, y_train)
+# coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=["Coefficient"])
+# print(coeff_df)
+# print(regressor.intercept_)
 
 
 sgdregressor = SGDRegressor(max_iter=1000)
@@ -36,9 +36,9 @@ print(sgdregressor.intercept_)
 
 
 # RESULTS AND EVALUATION
-y_pred = regressor.predict(X_test)
-print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
-print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
+# y_pred = regressor.predict(X_test)
+# print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
+# print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
 
 
 y_pred_sgd = sgdregressor.predict(X_test)
